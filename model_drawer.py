ab990ca6bfc302465c1a3fb6594fb76b50a12fed
Robert Chi <contact@robertchi.tw>
2020-07-26 22:12:54 +0800

    model_drawer.py: add epochs_metrics_plot()

--- model_drawer.py
+++ model_drawer.py
@@ -152,3 +152,22 @@ def cluster_drawer(x, y, centroids, title="", font='Arial Unicode MS'):
     plt.ylabel(x.columns[1])
     plt.legend(loc="best")
     plt.show()
+
+
+# In[] epochs_metrics_plot(): Draw the line plots of metrics for each epoch during Neural Net training
+def epochs_metrics_plot(history_dict, keys=(), title=None, xyLabel=[], ylim=(), size=()):
+  lineType = ("-", "--", ".", ":")
+  if len(ylim)==2:
+      plt.ylim(*ylim)
+  if len(size)==2:
+      plt.gcf().set_size_inches(*size)
+  epochs = range(1, len(history_dict[keys[0]])+1)
+  for i in range(len(keys)):
+      plt.plot(epochs, history_dict[keys[i]], lineType[i])
+  if title:
+      plt.title(title)
+  if len(xyLabel)==2:
+      plt.xlabel(xyLabel[0])
+      plt.ylabel(xyLabel[1])
+  plt.legend(keys, loc="best")
+  plt.show()
